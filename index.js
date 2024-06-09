99c13d2edd54ea6d46686515f0d53a29b0b33645
Lautaro Intraub <lauti.intraub@gmail.com>
2024-06-09 20:08:21 -0300

    Usando funciones y array

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let saldo = 0;
 
 let opcion;
 
+const movimientos = [];
 
 while (opcion != 0) {
 
@@ -12,6 +13,7 @@ while (opcion != 0) {
     *2. Ingresar dinero
     *3. Retiro parcial de dinero
     *4. Retiro total del dinero
+    *5. Ver ultimos 5 movimientos
     *0. Salir
 `);
 
@@ -27,12 +29,17 @@ while (opcion != 0) {
             break;
         case "3":
             const retiroParcial = Number(prompt('Cuanto dinero desea retirar: '));
+            agregarRetiro(retiroParcial);
             saldo -= retiroParcial;
             alert('Dinero retirado con éxito, su nuevo saldo es: ' + saldo);
             break;
         case "4":
+            agregarRetiro(saldo);
             saldo = 0;
             alert('Dinero retirado con éxito, su nuevo saldo es: ' + saldo);
+            break;
+        case "5":
+            alert('Los ultimos' + movimientos.length + 'retiros son: ' + verMovimientos());
             break;
         case "0":
             alert('Gracias por utilizar nuestro cajero');
@@ -41,4 +48,19 @@ while (opcion != 0) {
             alert('Ingrese una opción válida');
             break;
     }
+}
+
+
+function agregarRetiro(retiro) {
+    if (movimientos.length >= 5) {
+        movimientos.shift(); // Elimino el elemento más antiguo si ya hay 5 retiros
+    }
+    movimientos.push(retiro); // Agrego el nuevo retiro al final
+}
+
+function verMovimientos() {
+    if (movimientos.length === 0) {
+        return "No hay movimientos.";
+    }
+    return movimientos.join(', '); // Devuelvo los movimientos como una cadena separada por comas
 }
